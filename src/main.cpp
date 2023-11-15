5d086a5b5e93361aec9243e728cd74b4329fe52c
SinitinVladimir <vladimirsinitin@gmail.com>
2023-11-15 09:54:22 +0200

    U1 Creating: Canvas, Game Loop, Food and the Snake

--- src/main.cpp
+++ src/main.cpp
@@ -1,138 +1,126 @@
-#include <iostream>
-#include <raylib.h>
-#include <deque>
-#include <raymath.h>
+#include <iostream>     // standard I/O library for input and output operations
+#include <raylib.h>     // the Raylib library for graphics and game development
+#include <deque>        // the deque (double-ended queue) container from the standard library
+#include <raymath.h>    // Raylib's math functions for vector operations
 
-using namespace std;
+using namespace std;    // using standard library classes and functions without the 'std::' prefix
 
-Color green = {173, 204, 96, 255};
-Color darkGreen = {43, 51, 24, 255};
+Color green = {173, 204, 96, 255};         // a green color for the background
+Color darkGreen = {43, 51, 24, 255};       // a darker green color for the snake
 
-int cellSize = 30;
-int cellCount = 25;
+int cellSize = 30;      // the size of each cell in pixels
+int cellCount = 25;     // the number of cells in the grid
 
-double lastUpdateTime = 0;
-
+double lastUpdateTime = 0;   // the time of the last update for event handling
 
 bool eventTriggered(double interval)
 {
-    double currentTime = GetTime();
-    if (currentTime - lastUpdateTime >= interval)
+    double currentTime = GetTime();   // Retrieves the current time
+    if (currentTime - lastUpdateTime >= interval) // Checks if the specified interval has passed
     {
-        lastUpdateTime = currentTime;
-        return true;
+        lastUpdateTime = currentTime; // Updates the last update time
+        return true;                  // Returns true to trigger the event
     }
-    return false;
+    return false;                     // Otherwise, returns false
 }
 
 class Snake {
-
     public:
-        deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
-        Vector2 direction = {1, 0};
+        deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}}; // Initializes the snake's body as a deque of Vector2 positions
+        Vector2 direction = {1, 0};   // the initial direction of the snake
 
     void Draw() {
-        for(long unsigned int i = 0; i < body.size(); i++)
+        for(long unsigned int i = 0; i < body.size(); i++) // Iterates through each segment of the snake's body
         {
-            int x = body[i].x;
-            int y = body[i].y;
-            Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float)cellSize, (float)cellSize};
-            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
-//            DrawRectangle(x*cellSize, y*cellSize, cellSize, cellSize, darkGreen);
+            int x = body[i].x; // Retrieves the x-coordinate of the segment
+            int y = body[i].y; // Retrieves the y-coordinate of the segment
+            Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float)cellSize, (float)cellSize}; // Creates a rectangle for the segment
+            DrawRectangleRounded(segment, 0.5, 6, darkGreen); // Draws the rounded rectangle for the segment
         }
     }
 
     void Update()
     {
-        body.pop_back();
-        body.push_front(Vector2Add(body[0], direction));
+        body.pop_back(); // Removes the last segment of the body
+        body.push_front(Vector2Add(body[0], direction)); // Adds a new segment at the front in the current direction
     }
-
-
 };
-  
-
 
 class Food {
-
 public:
-Vector2 position;
-Texture2D texture;
+    Vector2 position;    // Stores the position of the food
+    Texture2D texture;   // Stores the texture of the food
 
-Food() 
-{
-    Image image = LoadImage("Graphics/Beluga_food.png");
-    texture = LoadTextureFromImage(image);
-    UnloadImage(image);
-    position = GenerateRandomPos();
-}
+    Food() 
+    {
+        Image image = LoadImage("Graphics/Beluga_food.png"); // Loads the food image
+        texture = LoadTextureFromImage(image); // Creates a texture from the loaded image
+        UnloadImage(image);                   // Frees the memory used by the image
+        position = GenerateRandomPos();       // Generates a random position for the food
+    }
 
-~Food()
-{
-        UnloadTexture(texture);
-}
+    ~Food()
+    {
+        UnloadTexture(texture); // Frees the memory used by the texture
+    }
 
-void Draw() 
-{
-    DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
-}
+    void Draw() 
+    {
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE); // Draws the food texture at the specified position
+    }
 
-Vector2 GenerateRandomPos() 
-{
-        float x = GetRandomValue(0, cellCount - 1);
-        float y = GetRandomValue(0, cellCount - 1);
-        return Vector2{x, y};
-}
+    Vector2 GenerateRandomPos() 
+    {
+        float x = GetRandomValue(0, cellCount - 1); // Generates a random x-value
+        float y = GetRandomValue(0, cellCount - 1); // Generates a random y-value
+        return Vector2{x, y};                       // Returns the new position as a Vector2
+    }
 };
 
 class Game
 {
 public:
-    Snake snake = Snake();
-    Food food = Food();
+    Snake snake = Snake(); // Creates an instance of the Snake class
+    Food food = Food();    // Creates an instance of the Food class
 
     void Draw()
     {
-        food.Draw();
-        snake.Draw();
+        food.Draw();       // Draws the food on the screen
+        snake.Draw();      // Draws the snake on the screen
     }
 
     void Update()
     {
-        snake.Update();
+        snake.Update();    // Updates the state of the snake
     }    
 
     void CheckCollisionWithFood()
     {
-        if(Vector2Equals(snake.body[0], food.position))
+        if(Vector2Equals(snake.body[0], food.position)) // Checks for collision between the snake's head and the food
         {
-            food.position = food.GenerateRandomPos();
+            food.position = food.GenerateRandomPos(); // Generates a new position for the food
         }
     }
 };
 
-
-
 int main()
 {
-    cout<<"Starting the game..."<<endl;
-    InitWindow(cellSize*cellCount, cellSize*cellCount, "Retro Snake");
-    SetTargetFPS(60); //Setting a preventive stable FPS so it won`t relaye on hardware capacity
+    cout<<"Starting the game..."<<endl; // Displays a start message
+    InitWindow(cellSize*cellCount, cellSize*cellCount, "Retro Snake"); // Initializes the game window
+    SetTargetFPS(60); // a stable target frame rate to not rely on hardware capacity
 
-    // Food food = Food();
-    // Snake snake = Snake();
-    Game game = Game();
+    Game game = Game(); // Creates a game instance
 
-
-    while(WindowShouldClose() == false)//WindowShouldClose exit the window whane Esc or X (section) is pressed
+    while(WindowShouldClose() == false) // Keeps running until the window is closed
     {
-        BeginDrawing(); //Create a black canvas
+        BeginDrawing(); // Starts the drawing process
 
-        if (eventTriggered(0.2))
+        if (eventTriggered(0.2)) // Checks if it's time to update the game state
         {
-            game.Update();
+            game.Update(); // Updates the game state
         }
 
+        // Checks for key presses and updates the snake's direction accordingly
         if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1)
         {
             game.snake.direction = {0, -1};
@@ -153,16 +141,12 @@ int main()
             game.snake.direction = {1, 0};
         }
 
+        ClearBackground(green); // Clears the background with the green color
+        game.Draw();            // Draws the game elements
 
-        //Drawing 
-        ClearBackground(green);
-        game.Draw();
-        // food.Draw();
-        // snake.Draw();
-
-        EndDrawing(); //Destoys the game window (tha black canvas)
+        EndDrawing();           // Ends the drawing process
     }
 
-    CloseWindow();
-     return 0;
-} 
+    CloseWindow(); // Closes the game window
+    return 0;
+}
